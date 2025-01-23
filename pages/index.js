87d0ba91a8e603e60a844ad70f5af52b1edcea42
Celine Delaugere <34991502+celdeldel@users.noreply.github.com>
2025-01-23 15:19:44 +0100

    Add use client directive

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+"use client";
+
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-100 py-12 px-4">
+    <main className="min-h-screen bg-gray-100 py-12 px-4">
       <h1>Hello World</h1>
-    </div>
+    </main>
   );
 }
